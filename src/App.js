fbbcc7552e42d51dd5442899ea19c3b48d20bff7
Nata010424 <106608212+Nata010424@users.noreply.github.com>
2022-11-24 22:43:01 +0200

    Added axios integration

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from "react";
 import Weather from "./Weather";
-
+import "./App.css";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Weather />
-        <p className="mt-2">
-          ✌️ This website was coded by Nataliia Mykhalevych, and is
-          <a href="https://github.com/Nata010424/React-weather-app">
-            {" "}
-            open-sourced
-          </a>
-        </p>
+        <div className="container">
+          <Weather defaultCity="Kyiv" />
+          <p className="mt-2">
+            ✌️ This website was coded by Nataliia Mykhalevych, and is
+            <a href="https://github.com/Nata010424/React-weather-app">
+              {" "}
+              open-sourced
+            </a>
+          </p>
+        </div>
       </header>
     </div>
   );
--- src/Weather.js
+++ src/Weather.js
@@ -1,65 +1,96 @@
-import React from "react";
+import React, { useState } from "react";
 import "./weather.css";
-export default function Weather() {
-  let weatherData = {
-    city: "Kyiv",
-    temperature: 14,
-    day: "Monday",
-    time: "13:00",
-    description: "cloudy",
-    imgUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
-    humidity: 65,
-    wind: 5,
-  };
-  return (
-    <div className="Weather">
-      <div className="search-city">
-        <div className="col-8 pe-3">
-          <form>
-            <input
-              type="text"
-              placeholder="Enter a city"
-              className="form-control"
-            />
-          </form>
-        </div>
-        <button className="btn" type="submit">
-          <i className="fa-solid fa-magnifying-glass second-icon"></i>
-        </button>
-        <button className="newbutton">Current</button>
-      </div>
-      <div className="row mt-5">
-        <div className="col-3">
-          <div className="first-block">
-            <h1>{weatherData.city}</h1>
-            <h2>{weatherData.day}</h2>
-            <h3>{weatherData.time}</h3>
-          </div>
-        </div>
-        <div className="col-3">
-          <div className="main-icon">
-            <img alt="Sunny" src={weatherData.imgUrl} />
-          </div>
-        </div>
+import axios from "axios";
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
 
-        <div className="col-3">
-          <div className="grade">
-            <span className="temp">{weatherData.temperature}</span>
-            <span className="units">
-              <a href="/" className="active">
-                °C
-              </a>{" "}
-              |<a href="/">°F</a>
-            </span>
-            <br />
-            <span>{weatherData.description}</span>
-            <br />
-            <span>humidity: {weatherData.humidity}</span>
-            <br />
-            <span>wind:{weatherData.wind} km/hour</span>
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      date: "Friday",
+      description: response.data.weather[0].description,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
+  function search() {
+    const apiKey = "ee87edddc26422e4cc399ddeca53c7c1";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <div className="search-city">
+          <div className="col-8 pe-3">
+            <form onSubmit={handleSubmit}>
+              <input
+                type="text"
+                placeholder="Enter a city"
+                className="form-control"
+                autoFocus="on"
+                onChange={handleCityChange}
+              />
+            </form>
+          </div>
+          <button className="btn" type="submit">
+            <i className="fa-solid fa-magnifying-glass second-icon"></i>
+          </button>
+          <button className="newbutton">Current</button>
+        </div>
+        <div className="row mt-5">
+          <div className="col-3">
+            <div className="first-block">
+              <h1>{weatherData.city}</h1>
+              <h2>{weatherData.day}</h2>
+              <h3>{weatherData.time}</h3>
+            </div>
+          </div>
+          <div className="col-3">
+            <div className="main-icon">
+              <img alt="Sunny" src={weatherData.imgUrl} />
+            </div>
+          </div>
+
+          <div className="col-3">
+            <div className="grade">
+              <span className="temp">
+                {Math.round(weatherData.temperature)}
+              </span>
+              <span className="units">
+                <a href="/" className="active">
+                  °C
+                </a>{" "}
+                |<a href="/">°F</a>
+              </span>
+              <br />
+              <span>{weatherData.description}</span>
+              <br />
+              <span>humidity: {weatherData.humidity}%</span>
+              <br />
+              <span>wind:{Math.round(weatherData.wind)}km/hour</span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    search();
+    return "Loading...";
+  }
 }
